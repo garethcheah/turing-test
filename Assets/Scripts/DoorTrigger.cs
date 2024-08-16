169f35e4657687b762aac791f49612f3fc22c387
garethcheah <garethcheah@gmail.com>
2024-08-16 01:14:30 -0700

    Initial commit including changes made during lesson 1 & 2 of Game Architecture & Development Overview

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private Animator _doorAnimator;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _doorAnimator = this.transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _doorAnimator.SetBool("isOpening", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _doorAnimator.SetBool("isOpening", false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement")]
+    [SerializeField] private float _playerSpeed;
+    [SerializeField] private float _sprintMultiplier;
+    [SerializeField] private float _turnSpeed;
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private bool _invertMouse;
+    [SerializeField] private float _jumpVelocity;
+    [SerializeField] private float _gravity = -9.81f;
+
+    [Header("Shooting")]
+    [SerializeField] private Rigidbody _bulletPrefab;
+    [SerializeField] private Transform _bulletSpawnPoint;
+    [SerializeField] private float _shootForce;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private float _groundCheckDistance;
+
+    private CharacterController _characterController;
+    private float _horizontal, _vertical;
+    private float _mouseX, _mouseY;
+    private float _cameraRotationX;
+    private float _moveMultiplier = 1.0f;
+    private Vector3 _playerVelocity;
+    private bool _isGrounded;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+
+        //Hides mouse cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetInput();
+        MovePlayer();
+        RotatePlayer();
+        GroundCheck();
+        JumpCheck();
+        ShootBullet();
+        Debug.DrawRay(_bulletSpawnPoint.position, _bulletSpawnPoint.forward, Color.red);
+    }
+
+    private void GetInput()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+        _mouseX = Input.GetAxis("Mouse X");
+        _mouseY = Input.GetAxis("Mouse Y");
+
+        _moveMultiplier = Input.GetButton("Fire3") ? _sprintMultiplier : 1.0f;
+    }
+
+    private void MovePlayer()
+    {
+        _characterController.Move((transform.forward * _vertical + transform.right * _horizontal) * _playerSpeed * Time.deltaTime * _moveMultiplier);
+
+        // Ground check
+        if (_isGrounded && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = -2.0f;
+        }
+
+        _playerVelocity.y += _gravity * Time.deltaTime;
+        _characterController.Move(_playerVelocity * Time.deltaTime);
+    }
+    
+    private void RotatePlayer()
+    {
+        // Turn player
+        transform.Rotate(Vector3.up * _turnSpeed * Time.deltaTime * _mouseX);
+
+        // Camera up and down
+        _cameraRotationX += Time.deltaTime * _mouseY * _turnSpeed * (_invertMouse ? 1 : -1);
+        _cameraRotationX = Mathf.Clamp(_cameraRotationX, -85.0f, 85.0f);
+
+        _cameraTransform.localRotation = Quaternion.Euler(_cameraRotationX, 0, 0);
+    }
+
+    private void GroundCheck()
+    {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundCheckDistance, _groundMask);
+    }
+
+    private void JumpCheck()
+    {
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _playerVelocity.y = _jumpVelocity;
+        }
+    }
+
+    private void ShootBullet()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody bullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, _bulletSpawnPoint.rotation);
+            bullet.AddForce(_cameraTransform.forward * _shootForce, ForceMode.Impulse);
+            Destroy(bullet.gameObject, 5.0f);
+        }
+    }
+}
